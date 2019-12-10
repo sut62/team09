ba2961cc913ad9c6c307e08e2f2e8f51df588543
Sanuntinee <sanuntineenink@gmail.com>
2019-12-10 18:11:22 +0700

    close #69 - ทำ Repository ของ Appointment

---
+++ backend/src/main/java/com/okta/springbootvue/Appointment/Repository/ClinicRepository.java
@@ -0,0 +1,11 @@
+package com.okta.springbootvue.Appointment.Repository;
+
+import com.okta.springbootvue.Appointment.Entity.Clinic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource  
+public interface ClinicRepository extends JpaRepository<Clinic, Long> {
+    Clinic findById(long id);
+}
