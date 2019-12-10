f9ab6c9c784a55f8a0b95c982e7e9dfdf86b8679
Sutanai <g.sniper046@gmail.com>
2019-12-10 16:02:43 +0700

    close #67 - add Repository RegisterDeaths

---
+++ backend/src/main/java/com/okta/springbootvue/RegisterDeaths/Repository/CauseofDeathRepository.java
@@ -0,0 +1,11 @@
+package com.okta.springbootvue.RegisterDeaths.Repository;
+
+import com.okta.springbootvue.RegisterDeaths.Entity.CauseofDeath;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CauseofDeathRepository extends JpaRepository<CauseofDeath, Long> {
+    CauseofDeath findById(long id);
+}
