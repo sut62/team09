9bf19d1b382259f8698ea1830d7e2af58ffe9954
mutita05 <mutita.b5910519@gmail.com>
2020-01-07 22:09:23 +0700

    close #32 - ทำหน้าจอ QueryUI สำหรับสอบถามอาการ

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+      <Query/>
+  </v-app>
+</template>
+
+<script>
+import Query from './components/Query';
+
+export default {
+  name: 'App',
+  components: {
+    Query,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
