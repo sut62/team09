685e3367676d755e638158ee8c7d507d280a8a43
mutita05 <mutita.b5910519@gmail.com>
2020-01-10 13:32:33 +0700

    close #126 -add backent Query

--- backend/src/main/java/com/okta/springbootvue/Query/Entity/Query.java
+++ backend/src/main/java/com/okta/springbootvue/Query/Entity/Query.java
@@ -1,6 +1,5 @@
 package com.okta.springbootvue.Query.Entity;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,7 +10,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import com.okta.springbootvue.Registerpatient.Repository.*;
 
 import com.okta.springbootvue.Query.Entity.Query;
 import com.okta.springbootvue.Registerpatient.Entity.*;
@@ -24,12 +22,12 @@ import lombok.NonNull;
 @Data
 @Entity
 @NoArgsConstructor
-@Table(name="QUERY")
+@Table(name = "QUERY")
 public class Query {
 
     @Id
-    @SequenceGenerator(name="query_seq",sequenceName="queryl_seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="queryl_seq")
+    @SequenceGenerator(name = "query_seq", sequenceName = "queryl_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "queryl_seq")
     @Column(name = "QUERY_ID", unique = true, nullable = true)
     private @NonNull Long queryid;
     private @NonNull Float temperature;
@@ -37,14 +35,14 @@ public class Query {
     private @NonNull Integer pressureDIA;
     private @NonNull String symptom;
 
-    private @NonNull Long registerId;
-	private @NonNull String firstName;
+    private @NonNull String firstName;
+
     private @NonNull String lastName;
 
-    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Duration.class)
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Registerpatient.class)
     @JoinColumn(name = "REGISTERPATIENT_ID", insertable = true)
-    private Registerpatient  registerpatient;
-    
+    private Registerpatient registerpatient;
+
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Duration.class)
     @JoinColumn(name = "DURATION_ID", insertable = true)
     private Duration duration;
@@ -52,6 +50,22 @@ public class Query {
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = CongenitalDisease.class)
     @JoinColumn(name = "CONGENITALDISEASE_ID", insertable = true)
     private CongenitalDisease congenitalDisease;
+
+    public Duration getDuration() {
+        return this.duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public CongenitalDisease getCongenitalDisease() {
+        return this.congenitalDisease;
+    }
+
+    public void setCongenitalDisease(CongenitalDisease congenitalDisease) {
+        this.congenitalDisease = congenitalDisease;
+    }
 
     @ManyToOne()
     @JsonBackReference
@@ -87,11 +101,36 @@ public class Query {
     public void setPressureDIA(Integer pressureDIA) {
         this.pressureDIA = pressureDIA;
     }
+
     public String getSymptom() {
         return symptom;
     }
 
     public void setSymptom(String symptom) {
         this.symptom = symptom;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Registerpatient getRegisterpatient() {
+        return this.registerpatient;
+    }
+
+    public void setRegisterpatient(Registerpatient registerpatient) {
+        this.registerpatient = registerpatient;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
