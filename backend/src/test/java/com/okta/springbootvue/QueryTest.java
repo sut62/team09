88c34f0fd8055792afb0027ae4c06321442bb203
mutita05 <mutita.b5910519@gmail.com>
2020-01-21 17:02:07 +0700

    close #164 - add test

--- backend/src/test/java/com/okta/springbootvue/QueryTest.java
+++ backend/src/test/java/com/okta/springbootvue/QueryTest.java
@@ -265,6 +265,7 @@ public class QueryTest {
         assertEquals("lastName", v.getPropertyPath().toString());
     }
 
+    
     @Test
     void B5910519_testFirstNameCannotEnterNumber() {
         Query query = new Query();
